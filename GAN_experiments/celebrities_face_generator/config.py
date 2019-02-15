2a0ae3221a91b4709bc33d3a5a247a7c0de489e2
Pierre Ecarlat <pierre.ecarlat@gmail.com>
2019-02-15 19:19:09 +0900

    Revert "Revert "Uploaded project to generate celebrities faces, torch version works fine (slow however, CPU here), and theres is a bug in tensorflow s version... TODO""
    This reverts commit 4302dcba18cef6b79abda6df1acdd4b7ec7316e9.

---
+++ GAN_experiments/celebrities_face_generator/config.py
@@ -0,0 +1,57 @@
+"""
+Configuration script, require easydict (`pip install easydict`).
+Can be imported using:
+`from config import cfg`
+"""
+
+import os
+from easydict import EasyDict as edict
+
+__C = edict()
+cfg = __C
+
+
+##############################################
+# General options                            #
+##############################################
+# Dataset to use (sample has 99 images)
+__C.DATASET_NAME = 'celeba_sample' # 'celeba'
+# Main directory
+__C.ROOT_DIR = os.path.dirname(__file__)
+# The directory where to find the datasets
+__C.DATASET_DIR = os.path.join(__C.ROOT_DIR, 'datasets', __C.DATASET_NAME)
+
+##############################################
+# Training options                           #
+##############################################
+# Number of workers for dataloader
+__C.WORKERS = 2
+# Batch size during training
+__C.BATCH_SIZE = 32
+# Number of training epochs
+__C.NB_EPOCHS = 100
+# Learning rate
+__C.LEARNING_RATE = 0.0002
+# Beta1 hyperparam for Adam optimizers
+__C.BETA1 = 0.5
+# Number of GPUs available. Use 0 for CPU mode.
+__C.NB_GPUS = 0
+
+##############################################
+# Images options                             #
+##############################################
+# Spatial size of training images. All images will be resized to this
+#   size using a transformer.
+__C.IMAGE_SIZE = 64
+# Number of channels (color images = 3, grey = 1)
+__C.NUMBER_CHANNELS = 3
+
+##############################################
+# GAN options                                #
+##############################################
+# Size of z latent vector (i.e. size of generator input)
+__C.SIZE_G_INPUT = 100
+# Size of feature maps in generator
+__C.SIZE_G_FEATURES_MAP = 64
+# Size of feature maps in discriminator
+__C.SIZE_D_FEATURES_MAP = 64
---
+++ GAN_experiments/celebrities_face_generator/test_torch.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python3
+
+import os
+import random
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+
+from config import cfg
+from generators import Generator_torch as G
+from discriminators import Discriminator_torch as D
+from utils import initializers
+from utils import visualizers
+
+
+# We can use an image folder dataset the way we have it setup.
+# Create the dataset
+preprocessing = transforms.Compose([
+    transforms.Resize(cfg.IMAGE_SIZE),
+    transforms.CenterCrop(cfg.IMAGE_SIZE),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+])
+dataset = dset.ImageFolder(root=cfg.DATASET_DIR, transform=preprocessing)
+
+# Create the dataloader
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=cfg.BATCH_SIZE,
+                                         shuffle=True, num_workers=cfg.WORKERS)
+
+# Decide which device we want to run on
+is_gpu = (torch.cuda.is_available() and cfg.NB_GPUS > 0)
+device = torch.device("cuda:0" if is_gpu else "cpu")
+
+# Create the generator and discriminator
+netG = G(cfg.NB_GPUS).to(device)
+netD = D(cfg.NB_GPUS).to(device)
+
+# Handle multi-gpu if desired
+if (device.type == 'cuda') and (ngpu > 1):
+    netG = nn.DataParallel(netG, list(range(ngpu)))
+    netD = nn.DataParallel(netD, list(range(ngpu)))
+
+# Apply the weights_init function to randomly initialize all weights
+#  to mean=0, stdev=0.2.
+netG.apply(initializers.weights_init)
+netD.apply(initializers.weights_init)
+
+# Initialize BCELoss function
+criterion = nn.BCELoss()
+
+# Create batch of latent vectors that we will use to visualize
+#  the progression of the generator
+fixed_noise = torch.randn(64, cfg.SIZE_G_INPUT, 1, 1, device=device)
+
+# Establish convention for real and fake labels during training
+real_label = 1
+fake_label = 0
+
+# Setup Adam optimizers for both G and D
+optimizerD = optim.Adam(netD.parameters(), lr=cfg.LEARNING_RATE,
+                                           betas=(cfg.BETA1, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=cfg.LEARNING_RATE,
+                                           betas=(cfg.BETA1, 0.999))
+
+# Training Loop
+
+# Lists to keep track of progress
+img_list = []
+G_losses = []
+D_losses = []
+iters = 0
+
+print("Starting Training Loop...")
+# For each epoch
+for epoch in range(cfg.NB_EPOCHS):
+    # For each batch in the dataloader
+    for i, data in enumerate(dataloader, 0):
+
+        ############################
+        # (1) Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+        ###########################
+        ## Train with all-real batch
+        netD.zero_grad()
+        # Format batch
+        real_cpu = data[0].to(device)
+        b_size = real_cpu.size(0)
+        label = torch.full((b_size,), real_label, device=device)
+        # Forward pass real batch through D
+        output = netD(real_cpu).view(-1)
+        # Calculate loss on all-real batch
+        errD_real = criterion(output, label)
+        # Calculate gradients for D in backward pass
+        errD_real.backward()
+        D_x = output.mean().item()
+
+        ## Train with all-fake batch
+        # Generate batch of latent vectors
+        noise = torch.randn(b_size, cfg.SIZE_G_INPUT, 1, 1, device=device)
+        # Generate fake image batch with G
+        fake = netG(noise)
+        label.fill_(fake_label)
+        # Classify all fake batch with D
+        output = netD(fake.detach()).view(-1)
+        # Calculate D's loss on the all-fake batch
+        errD_fake = criterion(output, label)
+        # Calculate the gradients for this batch
+        errD_fake.backward()
+        D_G_z1 = output.mean().item()
+        # Add the gradients from the all-real and all-fake batches
+        errD = errD_real + errD_fake
+        # Update D
+        optimizerD.step()
+
+        ############################
+        # (2) Update G network: maximize log(D(G(z)))
+        ###########################
+        netG.zero_grad()
+        label.fill_(real_label)  # fake labels are real for generator cost
+        # Since we just updated D, perform another forward pass of all-fake batch through D
+        output = netD(fake).view(-1)
+        # Calculate G's loss based on this output
+        errG = criterion(output, label)
+        # Calculate gradients for G
+        errG.backward()
+        D_G_z2 = output.mean().item()
+        # Update G
+        optimizerG.step()
+
+        # Output training stats
+        if i % 50 == 0:
+            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                  % (epoch, cfg.NB_EPOCHS, i, len(dataloader),
+                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+
+        # Save Losses for plotting later
+        G_losses.append(errG.item())
+        D_losses.append(errD.item())
+
+        # Check how the generator is doing by saving G's output on fixed_noise
+        if (iters % 500 == 0) or \
+           ((epoch == cfg.NB_EPOCHS-1) and (i == len(dataloader)-1)):
+            with torch.no_grad():
+                fake = netG(fixed_noise).detach().cpu()
+            img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+
+        iters += 1
+
+# Visulaization matplotlib (does not save anything)
+visualizers.plot_losses([
+    { 'name': 'Generator', 'loss': G_losses, },
+    { 'name': 'Discriminator', 'loss': D_losses, },
+])
+visualizers.generate_progression(img_list)
+visualizers.display_comparaison(next(iter(dataloader)), img_list, device)
